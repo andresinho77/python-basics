86ed34d62d756b62f2f594a5633a5dd8279d1af0
Andres Lara <andres.felipe.lara@gmail.com>
2023-01-10 17:47:07 +0100

    Adding arithmetic operations

---
+++ module_1/arithmetic-operators.py
@@ -0,0 +1,27 @@
+# Operators 
+a = int(input('Please input the first Number: '))
+b = int(input('Please input the second Number: '))
+
+#Sum 
+sum = a + b
+#Substraction
+subs = a - b
+#Multiplication 
+mult = a * b 
+#Division
+div= a/b
+#module
+mod = a%b
+#Pot
+p = a**b
+#floor
+f=a//b
+
+print('The answers are:')
+print('Sum:', sum)
+print('subs:', subs)
+print('mult:', mult)
+print('Div:', div)
+print('Mod:', mod)
+print('Potencia', p)
+print('Floor:', f)

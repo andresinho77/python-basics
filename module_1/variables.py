69a85ddfb5d459c66e9dc6801b6ab3ef078cad35
Andres Lara <andres.felipe.lara@gmail.com>
2023-01-10 16:01:02 +0100

    adding module_1

---
+++ module_1/variables.py
@@ -0,0 +1,33 @@
+# Variable Types
+# Types of comments
+"""
+Comentarios importantes en caso que se necesite
+"""
+#Variables
+a = 'a'
+A = 'A'
+b = 3023123
+c = 33
+
+# How to print variable
+print ('Hola Andres:')
+print (a , b)
+print(b * 3)
+print(A)
+
+#To Find variable Types
+print(type(a))
+print(type(A))
+print(type(b))
+
+# Variable Casting 
+x = str(5)
+y =float(c)
+print(type(x))
+print(type(y))
+
+#Multiple Variables
+aa,bb,cc = 3,2,1
+
+print(aa, bb, cc)
+

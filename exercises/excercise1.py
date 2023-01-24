fe4737d4130c86924e8574bc4f7550b43115b31f
Andres Lara <andres.felipe.lara@gmail.com>
2023-01-24 10:56:14 +0100

    adding some excercises

--- exercises/excercise1.py
+++ exercises/excercise1.py
@@ -1,3 +1,4 @@
+#EXCERCISE 1
 #Given two integer numbers return their product only if the product is equal to or lower than 1000, else return their sum.
 
 def calculate(num1, num2):
@@ -8,7 +9,13 @@ def calculate(num1, num2):
     else:
         print('The result is ', res)
 
-#Write a program to iterate the first 10 numbers and in each iteration, print the sum of the current and previous number.
+#print('********************')
+#num1=input('Insert numero 1: ')
+#num2=input('Insert numero 2: ')
+#calculate(num1, num2)
+
+#EXCERCISE 2
+#Write a script to iterate the first 10 numbers and in each iteration, print the sum of the current and previous number.
 
 def sum_iteration(i):
     x=0
@@ -17,12 +24,93 @@ def sum_iteration(i):
         print('Current Number', l, 'Previous Number', x, 'sum:', sum)
         x=l
 
-#print('********************')
-#num1=input('Insert numero 1: ')
-#num2=input('Insert numero 2: ')
-#calculate(num1, num2)
+# i=input('Insert numero de iteraciones:')
+# sum_iteration(int(i))
 
-i=input('Insert numero de iteraciones:')
-sum_iteration(int(i))
+#EXCERCISE 3
+#Write a program to accept a string from the user and display characters that are present at an even index number.
+def even_characters(word):
+    fin=int(len(word))
+    for i in range(fin):
+        if(i%2 == 0):
+            print('Character in position:', word[i])
+
+def even_characters2(word):
+    x = list(word)
+    for i in x[0::2]:
+        print(i)
+# palabra=input('Input the word:')
+# even_characters2(palabra)
+
+#EXERCISE 4
+#Write a program to remove characters from a string starting from zero up to n and return a new string.
+def remove_first_characters(word, num):
+    print('The word', word,':', word[num:])
+
+# palabra=input('Input the word:')
+# num=int(input('Input the number: '))
+# remove_first_characters(palabra,num)
+
+#EXCERCISE 5
+#Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+num_x=[30,40,30]
+num_y=[45,50,11]
+num_z=[45,50,11,213,123,213,123,123,123,12,312,3,123,123,12,31,23,123,1,23,12,45]
+
+def check_first_last(list):
+   if(list[0]==list[-1]):
+        print('True')
+   else:
+        print('False')
+
+# check_first_last(num_x)
+# check_first_last(num_y)
+# check_first_last(num_z)
+
+#EXCERCISE 6
+#Write a program to find how many times substring “Emma” appears in the given string.
+
+# string_x='Hola, soy andres y quiero ser andres el resto de andres, no importa cuantas veces diga andres'
+# print(string_x.count('andres'))
+
+#EXCERCISE 7
+#Print the following pattern
+# 1
+# 2 2
+# 3 3 3
+row = 12
+
+def print_pattern_7(i):
+    for j in range(i):
+        print(i, end=' ')
+    print('')
+
+for i in range(row):
+    print_pattern_7(i)
 
 
+#EXCERCISE 8
+#Print the following pattern
+# 1
+# 1 2
+# 1 2 3
+def print_pattern(i):
+    for j in range(1, i+1):
+        print(j, end=' ')
+    print('')
+
+for i in range(1,row + 1):
+    print_pattern(i)
+
+#EXCERCISE 9
+#Write a program to check if the given number is a palindrome number.
+
+def palindrom(number):
+    if(str(number) == str(number)[::-1]):
+        print('Palindrome')
+
+number = 744912335533219447
+# print(list(number))
+# print(str(number)[::-1])
+palindrom(number)

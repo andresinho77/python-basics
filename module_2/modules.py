46eec95784d98dd54fd548ed67825cceb82859c3
Andres Lara <andres.felipe.lara@gmail.com>
2023-01-29 13:02:58 +0100

    Adding modules in Module 2

---
+++ module_2/modules.py
@@ -0,0 +1,21 @@
+#Create a module
+
+def arithmetic(a , b):
+
+    add= a + b
+    sub= a - b
+    multiply= a * b
+    division=a/b
+    
+    return add, sub, multiply, division
+
+#red four return values in four variables
+a, b, c, d = arithmetic(11,3)
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
+

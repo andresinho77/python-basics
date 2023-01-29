ba5aa6a519586c5e03c3becf797e4a88148d517b
Andres Lara <andres.felipe.lara@gmail.com>
2023-01-29 12:46:54 +0100

    Adding Sets in Module 2

---
+++ module_2/sets.py
@@ -0,0 +1,16 @@
+#it is not required to keep the order using "SET" unlike lists
+
+#Sets
+frutas={'Perro','Gato','Mosquito','Mate','Pollo'}
+numero={1,23,123,4,57,887,877}
+print(frutas)
+
+for i in range(frutas):
+    print(frutas[i])
+
+#Add new values 
+frutas.add('Burro')
+print(frutas)
+
+
+

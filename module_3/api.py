77685b1b8df0bd4a28985269b64d71ba8dc9929c
Andres Lara <andres.felipe.lara@gmail.com>
2023-01-30 23:11:45 +0100

    Getting POST API Call

--- module_3/api.py
+++ module_3/api.py
@@ -1,10 +1,22 @@
 import requests
 
 
-endpoint = 'https://swapi.dev/api/planets/3/?format=json'
+endpoint = 'http://castlemock-env.eba-ashcqwxv.us-east-1.elasticbeanstalk.com/mock/rest/project/iOR4Js/application/tehwi6/'
 response = requests.get(endpoint)
 
 if(response.status_code == 200):
     print(response.json())
 else:
     print('Error 200')
+
+
+end='http://castlemock-env.eba-ashcqwxv.us-east-1.elasticbeanstalk.com/mock/rest/project/iOR4Js/application/tehwi6/'
+  
+# location given here
+location = "Netherlands"
+  
+# sending Post request and saving the response as response object
+r = requests.post(url = end)
+# extracting data in json format
+data = r.json()
+print(data)

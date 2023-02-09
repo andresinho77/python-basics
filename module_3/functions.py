c8cc5295dfc0a074bf4989741bd840fb76f06903
Andres Lara <andres.felipe.lara@gmail.com>
2023-02-09 19:54:14 +0100

    Adding Modules and Classes

--- module_3/functions.py
+++ module_3/functions.py
@@ -11,5 +11,22 @@ def add(b, *a):
         fib = fib + i * b
     print('La suma es: ',fib)
 
-add(100, 2,3,4)
+add(100,2,3,4)
+
+def calculateIMC(est, peso):
+    imc = peso / (est**2)
+    return imc
+
+def estadoPeso (imc):
+    
+    if (imc < 18.5):
+        estadoPeso = "Peso Bajo"
+    elif (imc > 18.5 and imc < 29.9):
+        estadoPeso = "Peso Normal"
+    else:
+       estadoPeso = "Sobre peso"
+    
+    return estadoPeso
+
+print('El IMC es igual a : ',estadoPeso(calculateIMC(1.74, 70)))
 

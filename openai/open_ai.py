118227d1dcd6fff28b1cd0497b7fc173f3349380
Andres Lara <andres.felipe.lara@gmail.com>
2023-03-13 19:02:54 +0100

    Extract the chatGPT response

--- openai/open_ai.py
+++ openai/open_ai.py
@@ -4,17 +4,26 @@ import json
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+prompt= str(input('Escriba la pregunta para Chat GPT:'))
+
 
 def cost_a(response):
     json_obj = json.loads(response)
+
+    ans = json_obj['choices']
     a = json_obj['usage']
-    print(a['prompt_tokens']*100)
-    print(a['total_tokens']*30)
-    # print(a[''])
+
+    print('Prompt_tokens:',a['prompt_tokens'])
+    print('Completion_tokens:' , a['completion_tokens'])
+    print('Total_tokens:', a['total_tokens'])
+    
+    print('Answer:', ans[0]['text'])
+    
+
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="hacer un guion de una pelicula sobre un plan para robar un banco" ,
+  prompt=prompt,
   temperature=0.5,
   max_tokens=1500,
   top_p=1.0,
